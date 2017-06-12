a324c03d45fd20144ccbf86bc0adb5b6c29e65c1
Xavi Matos <matos.xav@gmail.com>
2017-06-12 05:33:05 -0400

    point to CalQL8ed-K-OS version of Spectre

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Commander",
   dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+    .Package(url: "git@github.com:CalQL8ed-K-OS/Spectre.git", majorVersion: 0, minor: 8), 
   ]
 )
